177c4b40064c9528790d07d92fa02a4df95b0215
[CanhYuJie] <[980804224@qq.com]>
2016-10-28 11:22:55 +0800

    add jars,add appkey

---
+++ app/src/main/java/com/yujie/letwechat/SplashActivty.kt
@@ -0,0 +1,12 @@
+package com.yujie.letwechat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_activty)
+    }
+}
